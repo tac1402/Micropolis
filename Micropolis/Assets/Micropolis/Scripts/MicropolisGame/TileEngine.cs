822c11dd0fce77f5774af607b4c568bf1eef170d
Sergey <tac1402@gmail.com>
2024-09-11 02:29:58 +0300

    Экспортируем текстуры из тайлов, и создаем соответствующие материалы.

--- Micropolis/Assets/Micropolis/Scripts/MicropolisGame/TileEngine.cs
+++ Micropolis/Assets/Micropolis/Scripts/MicropolisGame/TileEngine.cs
@@ -27,23 +27,35 @@ namespace MicropolisGame
         /// <summary>
         /// Preload all tiles into a dictionary for fetching during draw mode
         /// </summary>
-        private void PreLoadTiles()
-        {
-            for (var i = 0; i < (int) MapTileCharacters.TILE_COUNT; i++)
-            {
-                var path = string.Format("Tiles/tiles_{0}", i);
-                var asset = Resources.Load<Tile>(path);
-                _tiles.Add(i, asset);
-            }
-        }
+		private void PreLoadTiles()
+		{
+			//TilemapToPng toPng = new TilemapToPng();
 
-        /// <summary>
-        /// Fetches a <see cref="Tile"/> object from a cache or adds it 
-        /// if it's being requested for the first time.
-        /// </summary>
-        /// <param name="tileId">Tile id to render from the MicroplisTileCharacter enum</param>
-        /// <returns>A <see cref="Tile"/> object that can be renedered onto a <see cref="Tilemap"/></returns>
-        public Tile GetTile(int tileId)
+			for (var i = 0; i < (int)MapTileCharacters.TILE_COUNT; i++)
+			{
+				var path = string.Format("Tiles/tiles_{0}", i);
+				Tile asset = Resources.Load<Tile>(path);
+
+				/*
+                //toPng.ExportAsPng(toPng.TextureFromSprite(asset.sprite), string.Format("tiles_{0}", i));
+                Material m = new Material(Shader.Find("Standard"));
+                string p = "Assets/Resources/Exported Tilemaps/" + string.Format("tiles_{0}", i) + ".png";
+                Texture2D t = (Texture2D)AssetDatabase.LoadAssetAtPath(p, typeof(Texture2D));
+				m.mainTexture =  t;
+                toPng.ExportAsMaterial(m, string.Format("m_{0}", i));
+                */
+
+				_tiles.Add(i, asset);
+			}
+		}
+
+		/// <summary>
+		/// Fetches a <see cref="Tile"/> object from a cache or adds it 
+		/// if it's being requested for the first time.
+		/// </summary>
+		/// <param name="tileId">Tile id to render from the MicroplisTileCharacter enum</param>
+		/// <returns>A <see cref="Tile"/> object that can be renedered onto a <see cref="Tilemap"/></returns>
+		public Tile GetTile(int tileId)
         {
             if (_tiles.ContainsKey(tileId))
             {
